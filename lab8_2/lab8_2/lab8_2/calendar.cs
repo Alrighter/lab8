cdd0521c9b21c61608c8f6499a3de3d423d2d8df
Alrighter <59889888+Alrighter@users.noreply.github.com>
2020-12-03 23:26:29 +0200

    m

---
+++ lab8_2/lab8_2/lab8_2/calendar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab8_2
+{
+    class calendar
+    {
+        public calendar(){}
+
+        private int day;
+        private int month;
+        private int year;
+    }
+}
